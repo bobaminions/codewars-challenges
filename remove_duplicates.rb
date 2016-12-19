9b81eca2fb91c689ed8ff5224c0799fa7c04dd09
Ramesh Thakulla <bobaminions@gmail.com>
2016-12-19 17:43:25 +0000

    Comments added

--- remove_duplicates.rb
+++ remove_duplicates.rb
@@ -2,7 +2,7 @@
 def unique(integers)
   # Initializing empty array
   unique_array = []
-  # Looping / iterating through each value of array integers
+  # Looping / iterating through each value of array 'integers'
   integers.each do |i|
     # Add 'i' item in integers to 'unique_array' using 'push' method
     # until or unless 'i' is not already included in 'unique_array'
