cbd41811ebbd8bdfa82eff10a37eb9d94740a171
Ramesh Thakulla <bobaminions@gmail.com>
2016-12-19 20:19:38 +0000

    Add add item in array

---
+++ add_item_in_array.rb
@@ -0,0 +1,5 @@
+# add the value "codewars" to the already defined websites array
+websites.push("codewars")
+
+# add the value "codewars" to the already defined websites array
+websites[0] = "codewars"
