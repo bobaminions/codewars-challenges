78374509f6642b60a41527b313be715f308b240c
Ramesh Thakulla <bobaminions@gmail.com>
2016-12-20 07:18:02 +0000

    Refactor rock scissor paper

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -8,3 +8,17 @@ def rps(p1, p2)
     "Draw!"
   end
 end
+
+# Refactoring code with array of players
+
+def rps(p1, p2)
+  #your code here
+  player = ["rock", "scissors", "paper"]
+  if (p1 == player[0] and p2 == player[1]) or (p1 == player[1] and p2 == player[2]) or (p1 == player[2] and p2 == player[0])
+    "Player 1 won!"
+  elsif (p1 == player[1] and p2 == player[0]) or (p1 == player[2] and p2 == player[1]) or (p1 == player[0] and p2 == player[2])
+    "Player 2 won!"
+  else
+    "Draw!"
+  end
+end
